c5bfb404dd62dbdff1925783b9880a168ec1f8e9
Andrew Burnett <andrew@andburn.info>
2017-03-08 21:00:15 +0000

    Handle tags in description text

--- generate.py
+++ generate.py
@@ -2,6 +2,7 @@
 
 import sys
 import math
+import re
 import json
 import os.path
 from operator import itemgetter, attrgetter
@@ -10,7 +11,9 @@ import cairo
 from gi.repository import Pango
 from gi.repository import PangoCairo
 from hearthstone.cardxml import load
-from hearthstone.enums import CardType, CardSet, MultiClassGroup, get_localized_name
+from hearthstone.enums import (
+	CardType, CardSet, MultiClassGroup, Locale, get_localized_name
+)
 
 from neferset.curved import CubicBezier, CurvedText, curved_text
 from neferset.drawing import (
@@ -79,6 +82,76 @@ def render_component(context, component, data):
 		if hasattr(neferset.custom, component.custom["name"]):
 			func = getattr(neferset.custom, component.custom["name"])
 			func(context, component, data.data)
+
+
+def plural_index(num, locale):
+	if locale == Locale.ruRU:
+		mod = num % 100
+		if mod in range(11, 15):
+			return 2
+		else:
+			mod = num % 10
+			if mod == 1:
+				return 0
+			elif mod in range(2, 5):
+				return 1
+			else:
+				return 2
+	elif locale == Locale.plPL:
+		if num == 1:
+			return 0
+		elif num == 0:
+			return 2
+		else:
+			mod = num % 100
+			if mod in range(11, 15):
+				return 2
+			else:
+				mod = num % 10
+				if mod in range(2, 5):
+					return 1;
+				else:
+					return 2;
+	elif num <= 1:
+		return 0
+	return 1
+
+
+def format_plurals(text, locale):
+	pattern = re.compile(r"(\d+)(.*?)(\|4\(([^,]+(,[^,]+?)*)\))")
+	match = pattern.search(text)
+	while match:
+		num = int(match.group(1))
+		words = match.group(4).split(",")
+		idx = plural_index(num, locale)
+		text = text[:match.start(3)] + words[idx] + text[match.end(3):]
+		match = pattern.search(text, match.end(3))
+	return text
+
+
+def clean_description_text(text, locale):
+	"""Remove the non-markup tags from the card description text.
+
+	$, #	positive, negative multipliers (removed)
+	_ 		non-breaking space (removed)
+	[x]		disable automatic wrapping, can occur mid text (handled)
+	[d]		indicates hyphenation is possible, deDE only (removed)
+	[b]		unknown, jaJP and thTH only, maybe line break is allowed (removed)
+	|4()	plurals
+	@		dynamic/in-game text appears before the @, static text after
+	"""
+	if "@" in text:
+		text = text.split("@")[1]
+	text = format_plurals(text, locale)
+	idx = text.find("[x]")
+	if idx == 0:
+		text = text.replace("[x]", "")
+	elif idx > 0:
+		text = re.sub(r"\n", " ", text[:idx + 3]) + text[idx + 3:]
+		text = re.sub(r"\s*\[x\]", "\n", text)
+	text = re.sub(r"[$#]|\[b\]|\[d\]", "", text)
+	text = re.sub(r"[_ ]+", " ", text)
+	return text
 
 
 def setup_context(width, height):
@@ -156,7 +229,7 @@ def main():
 		elif c.type == ComponentType.base:
 			cdata = ComponentData("default")
 		elif c.type == ComponentType.description:
-			cdata = ComponentData("default", card.description)
+			cdata = ComponentData("default", clean_description_text(card.description, Locale[locale]))
 		elif c.type == ComponentType.cardSet:
 			# TODO need to rework theme dir here and elsewehre
 			# TODO pass on premium state, taken from input?
