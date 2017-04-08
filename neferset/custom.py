c520faa65951ba9fff3c5e7e8270db91599dbe1d
Andrew Burnett <andrew@andburn.info>
2017-04-08 12:36:05 +0100

    Add custom svg set function for pure

--- neferset/custom.py
+++ neferset/custom.py
@@ -129,3 +129,57 @@ def set_watermark(ctx, comp, data):
 	out.close()
 	descp_img.close()
 	set_img.close()
+
+SET_SVGS = {}
+
+def set_rarity_svg(ctx, comp, data):
+	from lxml import etree
+	from gi.repository import Rsvg
+
+	file_ext = ".svg"
+	scale = comp.custom["region"]["width"] / 128;
+	colors = {
+		Rarity.COMMON: "#8C8C8C",
+		Rarity.RARE: "#277FFF",
+		Rarity.EPIC: "#9828BB",
+		Rarity.LEGENDARY: "#FF8800"
+	}
+	card = data["card"]
+	theme_dir = os.path.join(data["dir"], comp.custom["setIcons"])
+	set_name = card.card_set.name.lower()
+
+	# first call, populate dict
+	if len(SET_SVGS) == 0:
+		for s in CardSet:
+			name = s.name.lower()
+			icon = os.path.join(theme_dir, "{}{}".format(name, file_ext))
+			if os.path.isfile(icon):
+				SET_SVGS[name] = etree.parse(icon)
+		print("{} set icons loaded".format(len(SET_SVGS)))
+
+	# get the position
+	set_region = Region(
+		comp.custom["region"]["x"],
+		comp.custom["region"]["y"],
+		comp.custom["region"]["width"],
+		comp.custom["region"]["height"])
+	# check the svg exists
+	if set_name not in SET_SVGS:
+		print("Warning: set icon not found for '{}'".format(set_name))
+		return
+	# get the svg and switch the color
+	if card.rarity in colors:
+		SET_SVGS[set_name].getroot().attrib["fill"] = colors[card.rarity]
+	else:
+		print("{}, no color found for rarity {}".format(card.id, card.rarity))
+		return
+
+	ctx.save()
+	ctx.new_path()
+	ctx.translate(set_region.x, set_region.y)
+	ctx.scale(scale, scale)
+	handle = Rsvg.Handle.new()
+	handle.write(etree.tostring(SET_SVGS[set_name]))
+	handle.close()
+	Rsvg.Handle.render_cairo(handle, ctx)
+	ctx.restore();
