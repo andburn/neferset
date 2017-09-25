4808b4e610f5ad599e20c575761328055b4e2373
Andrew Burnett <andrew@andburn.info>
2017-09-25 11:53:28 +0000

    Add setup bootstrap

---
+++ bootstrap.ps1
@@ -0,0 +1,27 @@
+
+$BASEDIR = $PSScriptRoot
+$HSDATA_URL = "https://github.com/HearthSim/hsdata.git"
+$HSDATA_DIR = "$BASEDIR/hsdata"
+$HEARTHFORGE_URL = "https://github.com/HearthSim/hearthforge.git"
+$HEARTHFORGE_DIR = "$BASEDIR/assets"
+
+Function FetchGit() {
+    Param (
+        [string]$src,
+        [string]$dest
+    )
+	Write-Host "Fetching data files from $src"
+	if (-not (Test-Path $dest)) {
+		& git clone --depth=1 "$src" "$dest"
+    } else {
+		& git -C "$dest" fetch
+		& git -C "$dest" reset --hard origin/master
+    }
+}
+
+if (Get-Command "git.exe" -ErrorAction SilentlyContinue) {
+    FetchGit $HSDATA_URL $HSDATA_DIR
+    FetchGit $HEARTHFORGE_URL $HEARTHFORGE_DIR
+} else {
+    Write-Host -ForegroundColor Red "ERROR: git is required to bootstrap this project."
+}
